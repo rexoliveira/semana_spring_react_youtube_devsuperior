945e30761e0eeae5ab09e6bcfbdc79f020229ea4
rexoliveira <roolivei@yahoo.com.br>
2022-11-17 12:56:04 -0300

    Other Compnents

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 
 function App() {
   return (
-    <><h1>Olá Mundo!</h1>
-    <NotificationButton />
-    <NotificationButton />
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div >
+        </section >
+      </main >
     </>
   )
 }
